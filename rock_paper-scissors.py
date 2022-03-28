22abfd9cae6e84bb43572c6221402df587af5bce
Vaidh <vedprakashsingh216@gmail.com>
2022-03-28 22:39:51 +0530

    commit

--- rock_paper-scissors.py
+++ rock_paper-scissors.py
@@ -3,7 +3,7 @@ import random
 user_wins = 0
 computer_wins = 0
 
-options = ['rock','paper','scissors']
+options = ["rock", "paper", "scissors"]
 
 while True:
     user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
@@ -11,7 +11,7 @@ while True:
     if user_input == "q":
         break
     
-    if user_input not in ["rock", "paper", "scissor"]:
+    if user_input not in options:
         continue
 
     random_number = random.randint(0,2)
@@ -19,7 +19,7 @@ while True:
     computer_pick = options[random_number]
     print(f"Computer picked {computer_pick}.")
 
-    if user_input == "rock" and computer_pick == "scissor":
+    if user_input == "rock" and computer_pick == "scissors":
         print("You won!")
         user_wins += 1
         continue
@@ -29,14 +29,28 @@ while True:
         user_wins += 1
         continue
 
-    elif user_input == "scissors" and computer_pick == "paper ":
+    elif user_input == "scissors" and computer_pick == "paper":
         print("You won!")
         user_wins += 1
         continue
+    
+    elif user_input == "scissors" and computer_pick == "rock":
+        print("You Lost!")
+        computer_pick += 1
+        continue
 
-    else:
+    elif user_input == "paper" and computer_pick == "scissors":
         print("You lost!")
         computer_wins += 1
+        continue
+
+    elif user_input == "rock" and computer_pick == "paper":
+        print("You lost!")
+        computer_wins += 1
+        continue
+
+    else:
+        print("Match draw!")
 
 print(f"User wins {user_wins} times.")
 print(f"Computer wins {computer_wins} times")
