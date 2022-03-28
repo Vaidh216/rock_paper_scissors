34c767106f7997fe77574ecb923f5207e4431ae1
Vaidh <vedprakashsingh216@gmail.com>
2022-03-28 22:28:50 +0530

    commit

--- rock_paper-scissors.py
+++ rock_paper-scissors.py
@@ -3,16 +3,40 @@ import random
 user_wins = 0
 computer_wins = 0
 
+options = ['rock','paper','scissors']
+
 while True:
     user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
 
     if user_input == "q":
-        break()
+        break
     
-    if user_input not in ["rock", "paper", "scissors"]:
+    if user_input not in ["rock", "paper", "scissor"]:
         continue
 
     random_number = random.randint(0,2)
     #rock:0, paper: 1, scissors:2
+    computer_pick = options[random_number]
+    print(f"Computer picked {computer_pick}.")
 
-    print("hello")
+    if user_input == "rock" and computer_pick == "scissor":
+        print("You won!")
+        user_wins += 1
+        continue
+
+    elif user_input == "paper" and computer_pick == "rock":
+        print("You won!")
+        user_wins += 1
+        continue
+
+    elif user_input == "scissors" and computer_pick == "paper ":
+        print("You won!")
+        user_wins += 1
+        continue
+
+    else:
+        print("You lost!")
+        computer_wins += 1
+
+print(f"User wins {user_wins} times.")
+print(f"Computer wins {computer_wins} times")
